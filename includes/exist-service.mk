c9c9eeb77ffc4c535830ca937424b611afcb4957
grantmacken <grantmacken@gmail.com>
2016-01-06 15:16:28 +1300

    add exist service

---
+++ includes/exist-service.mk
@@ -0,0 +1,30 @@
+
+
+$(EXIST_SERVICE): $(EXPECT_LOG)
+	$(if $(shell ps -p1 | grep systemd ),\
+ $(info  OK init system is systemd),\
+ $(error init system is not systemd) )
+	@echo "## $(notdir $@) ##"
+	@$(call assert-is-root)
+	@systemctl is-failed exist.service > /dev/null && echo 'OK! unit intentionally stopped'
+	$(file > $(@),[Unit])
+	$(file >> $(@),Description=The exist db application server)
+	$(file >> $(@),After=network.target)
+	$(file >> $(@),)
+	$(file >> $(@),[Service])
+	$(file >> $(@),Enviroment="EXIST_HOME=$(EXIST_HOME)")
+	$(file >> $(@),$(if $(SUDO_USER),Enviroment="SERVER=development",Enviroment="SERVER=production"))
+	$(file >> $(@),WorkingDirectory=$(EXIST_HOME))
+	$(file >> $(@),User=$(INSTALLER))
+	$(file >> $(@),Group=$(INSTALLER))
+	$(file >> $(@),ExecStart=$(START_JAR) jetty)
+	$(file >> $(@),ExecStop=$(START_JAR) shutdown -u admin -p $(P) )
+	$(file >> $(@),)
+	$(file >> $(@),[Install])
+	$(file >> $(@),WantedBy=multi-user.target)
+	@cp $@ /lib/systemd/system/$(notdir $@)
+	@systemd-analyze verify $(notdir $@)
+	@systemctl enable  $(notdir $@)
+	@systemctl start  $(notdir $@)
+	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
+	@echo '-------------------------------------------------------------------'
