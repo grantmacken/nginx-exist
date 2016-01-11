0eba026fb1b0ab10da36cb34581429f43211d1ef
grantmacken <grantmacken@gmail.com>
2016-01-12 09:02:28 +1300

    test nginx install

--- includes/nginx-install.mk
+++ includes/nginx-install.mk
@@ -24,45 +24,43 @@ $(NGINX_VERSION):
 	@echo NGINX_VER=$$( curl -s -L  $(NGINX_VERSION_SOURCE) | tr -d '\n\r' |\
  grep -oP 'nginx-\K([0-9]+\.[0-9]+\.[0-9]+)(?=....$(NGINX_VERSION_OPTION))' |\
  head -1) > $(@)
+	@cat $(@) | tail -1
 	@echo 'fetch the latest pcre version'
 	@echo PCRE_VER=$$( \
  curl -s 'ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/' | \
  grep -oP "pcre-\K[0-9]+\.[0-9]+(?=\.tar\.gz)" | \
  tail -1  ) >> $(@)
+	@cat $(@) | tail -1
 	@echo 'fetch the latest zlib version'
 	@echo ZLIB_VER=$$( \
   curl -s 'http://zlib.net' | \
   grep -oP 'zlib \K[0-9]+\.[0-9]+\.[0-9]+' | \
   head -1 ) >> $(@)
-	@echo 'fetch the latest opnssl version'
-	@echo OPENSSL_VER=$$( \
- curl -s 'https://www.openssl.org/source/' |  \
- grep -oP '>openssl-\K[0-9a-z\.]+(?=\.tar\.gz)' |  \
- tail -1 ) >> $(@)
+	@cat $(@) | tail -1
 	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
 	@echo '-----------------------------------------------------------------}}}'
 
 $(TEMP_DIR)/curl-nginx.log: $(NGINX_VERSION)
 	@echo "{{{ $(notdir $@) "
-	@echo "$(call getVERSION,$<,nginx)"
-	@curl $(NGINX_DOWNLOAD)/$(call getVERSION,$<,nginx) |  \
+	@echo "$(NGINX_DOWNLOAD)/$(call getVERSION,$<,nginx)" && \
+ curl $(NGINX_DOWNLOAD)/$(call getVERSION,$<,nginx) |  \
  tar xz --directory $(dir $@) && \
- echo  'downloaded and unzipped $(call getVERSION,$<,nginx)' >  $(@) &&  \
+ echo "$(ZLIB_DOWNLOAD)/$(call getVERSION,$<,zlib)" && \
  curl $(ZLIB_DOWNLOAD)/$(call getVERSION,$<,zlib) | \
  tar xz --directory $(dir $@) && \
- echo  'downloaded and unzipped $(call getVERSION,$<,zlib)' >>  $(@) &&  \
+ echo "$(PCRE_DOWNLOAD)/$(call getVERSION,$<,pcre)" && \
  curl $(PCRE_DOWNLOAD)/$(call getVERSION,$<,pcre) | \
- tar xz --directory $(dir $@) && \
- echo  'downloaded and unzipped $(call getVERSION,$<,pcre)' >>  $(@) &&  \
- curl $(OPENSSL_DOWNLOAD)/$(call getVERSION,$<,openssl) | \
- tar xz --directory $(dir $@) && \
- echo  'downloaded and unzipped $(call getVERSION,$<,openssl)' >>  $(@)
+ tar xz --directory $(dir $@)
+	@echo  'downloaded and unzipped $(call getVERSION,$<,nginx)' >  $(@) 
+	@echo  'downloaded and unzipped $(call getVERSION,$<,zlib)' >>  $(@) 
+	@echo  'downloaded and unzipped $(call getVERSION,$<,pcre)' >>  $(@)
+	@echo  'downloaded and unzipped $(call getVERSION,$<,openssl)' >>  $(@)
 	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
 	@echo '-----------------------------------------------------------------}}}'
 
 $(NGINX_HOME)/sbin/nginx: $(TEMP_DIR)/curl-nginx.log
 	@echo "{{{ $(notdir $@) "
-	source $(NGINX_VERSION); cd $(dir $(@))/nginx-$${NGINX_VER} ;\
+	source $(NGINX_VERSION); cd $(dir $(<))/nginx-$${NGINX_VER} ;\
  ./configure   --with-select_module  \
  --with-pcre="../pcre-$${PCRE_VER}" \
  --with-zlib="../zlib-$${ZLIB_VER}" \
@@ -70,17 +68,27 @@ $(NGINX_HOME)/sbin/nginx: $(TEMP_DIR)/curl-nginx.log
 	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
 	@echo '-----------------------------------------------------------------}}}'
 
-$(TEMP_DIR)/nginx_ssl_install.log: $(TEMP_DIR)/curl-nginx.log
-	@echo "{{{ $(notdir $@) "
-	source $(NGINX_VERSION); cd $(dir $(@))/nginx-$${NGINX_VER} ;\
- ./configure   --with-select_module  \
- --with-pcre="../pcre-$${PCRE_VER}" \
- --with-http_ssl_module \
- --with-openssl="../openssl-$${OPENSSL_VER}" \
- --with-zlib="../zlib-$${ZLIB_VER}" \
- --with-http_gzip_static_module && make && make install
-	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
-	@echo '-----------------------------------------------------------------}}}'
+# $(TEMP_DIR)/nginx_ssl_install.log: $(TEMP_DIR)/curl-nginx.log
+# @echo 'fetch the latest opnssl version'
+# @echo OPENSSL_VER=$$( \
+# curl -s 'https://www.openssl.org/source/' |  \
+# grep -oP '>openssl-\K[0-9a-z\.]+(?=\.tar\.gz)' |  \
+# tail -1 ) >> $(@)
+ # curl $(OPENSSL_DOWNLOAD)/$(call getVERSION,$<,openssl) | \
+ # tar xz --directory $(dir $@)
+ # echo "$(PCRE_DOWNLOAD)/$(call getVERSION,$<,pcre)" && \
+ # curl $(OPENSSL_DOWNLOAD)/$(call getVERSION,$<,openssl) | \
+ # tar xz --directory $(dir $@)
+# @echo "{{{ $(notdir $@) "
+# source $(NGINX_VERSION); cd $(dir $(@))/nginx-$${NGINX_VER} ;\
+# ./configure   --with-select_module  \
+# --with-pcre="../pcre-$${PCRE_VER}" \
+# --with-http_ssl_module \
+# --with-openssl="../openssl-$${OPENSSL_VER}" \
+# --with-zlib="../zlib-$${ZLIB_VER}" \
+# --with-http_gzip_static_module && make && make install
+# @$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
+# @echo '-----------------------------------------------------------------}}}'
 
 $(TEMP_DIR)/nginx-run.sh: $(NGINX_HOME)/sbin/nginx
 	@echo "{{{ $(notdir $@) "
