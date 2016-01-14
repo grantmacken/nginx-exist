727f125ad6e4d8dffaa3df60325ea4ff531d18c8
grantmacken <grantmacken@gmail.com>
2016-01-14 14:27:22 +1300

    add automated webdav install

--- includes/eXist-install.mk
+++ includes/eXist-install.mk
@@ -87,7 +87,7 @@ $(TEMP_DIR)/eXist-run.sh: $(TEMP_DIR)/eXist-expect.log
 	@echo '-------------------------------------------------------------------'
 
 $(TEMP_DIR)/exist.service: $(TEMP_DIR)/eXist-expect.log
-	@echo "## $(notdir $@) ##"
+	@echo "{{{  $(notdir $@) "
 	$(if $(shell ps -p1 | grep systemd ),\
  $(info  OK init system is systemd),\
  $(error init system is not systemd) )
@@ -113,4 +113,54 @@ $(TEMP_DIR)/exist.service: $(TEMP_DIR)/eXist-expect.log
 	@systemctl enable  $(notdir $@)
 	@systemctl start  $(notdir $@)
 	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
-	@echo '-------------------------------------------------------------------'
+	@echo '-----}}}'
+
+webdav:  $(TEMP_DIR)/webdav.log
+
+$(TEMP_DIR)/webdav.expect:
+	@echo "{{{ $(notdir $@) ##"
+	@echo 'creating webdav expect file'
+	@echo '#!$(EXPECT) -f' > $(@)
+	@echo 'spawn dpkg-reconfigure davfs2 -freadline' >> $(@)
+	@echo 'expect "Should" { send "Y\n" }'  >> $(@)
+	@echo 'expect -timeout -1 "done" {' >> $(@)
+	@echo ' wait'  >> $(@)
+	@echo ' exit'  >> $(@)
+	@echo '}'  >> $(@)
+	@chmod +x $(@)
+	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
+	@echo '---}}}'
+
+$(TEMP_DIR)/webdav.log: $(TEMP_DIR)/webdav.expect
+	@echo '{{{ $(notdir $@) '
+	@$(info CHECK -  mount.davfs suid flag set for user, allowing user to mount webdav)
+	@$(if $(shell test -u /usr/sbin/mount.davfs && echo 'true'),\
+ $(info OK! mount.davfs suid flag set),\
+ $< &&  test -u /usr/sbin/mount.davfs && echo 'set suid for mount.davfs')
+	@$(info CHECK -  if there is a davfs group )
+	@$(if $(shell echo "$$(groups davfs2 2>/dev/null)"),\
+ $(info OK! there is davfs2 group),\
+ groupadd davfs2 && usermod -aG davfs2 $(SUDO_USER) && groups davfs2 )
+	@$(info CHECK -  if user belongs to davfs2 group )
+	@$(if $(shell echo "$$(id gmack 2>/dev/null | grep -oP '(\Kdavfs2)')"),\
+ $(info OK! user belongs to davfs2 group),\
+ usermod -aG davfs2 $(SUDO_USER) && echo 'Need to refresh group membership by *logging out* ' )
+	@$(info CHECK -   have $(HOME)/eXist davfs mount point in fstab)
+	@$(if $(shell echo "$$(cat /etc/fstab | grep $(HOME)/eXist )"),\
+ $(info OK! have davfs mount point in fstab),\
+ echo "http://localhost:8080/exist/webdav/db  $(HOME)/eXist  _netdev,user,rw,noauto  0  0" >> /etc/fstab )
+	@$(if $(shell echo "$$( mount | grep -oP '$(HOME)/eXist' )"),\
+  umount $(HOME)/eXist,\
+ $(info not yet mounted))
+	@echo '#very very very secret' >  /etc/davfs2/secrets
+	@echo '/home/$(SUDO_USER)/eXist admin  $(P)' >> /etc/davfs2/secrets
+	@chmod -v 600 /etc/davfs2/secrets
+	@if [ ! -d $(HOME)/eXist ] ; then mkdir $(HOME)/eXist ; fi
+	@if [ ! -d $(HOME)/.davfs2 ] ; then mkdir $(HOME)/.davfs2 ; fi
+	@cp /etc/davfs2/davfs2.conf $(HOME)/.davfs2/davfs2.conf
+	@cp /etc/davfs2/secrets $(HOME)/.davfs2/secrets
+	@chown -v $(SUDO_USER):davfs2 $(HOME)/.davfs2/*
+	@ls -al $(HOME)/.davfs2
+	@su -c "mount $(HOME)/eXist" -s /bin/sh $(INSTALLER)
+	@su -c "mount | grep -oP '^.+\s$(HOME)/eXist'" -s /bin/sh $(INSTALLER)
+	@echo '-------------}}} '
--- t/test.t
+++ t/test.t
@@ -2,13 +2,16 @@
 source t/setup
 use Test::More
 
-plan tests 5
+plan tests 7
 
 note "test plan for ngnix-exist"
 
 ok "$( [ -n ${EXIST_HOME} ] )"  "eXist home set: ${EXIST_HOME}"
 
 ok "$( [[ -n "$(curl -I -s -f 'http://127.0.0.1:8080/')" ]] )"  'eXist is reachable'
+
+is "$(curl -Is http://127.0.0.1:8080/ |\
+ grep -oP 'Jetty')" 'Jetty'  'Jetty serves on port 8080' 
 
 is "$(cd ${EXIST_HOME};echo 'sm:is-authenticated()' |\
  java -jar ${EXIST_HOME}/start.jar client -sqx -u admin -P ${P} |\
@@ -17,6 +20,11 @@ is "$(cd ${EXIST_HOME};echo 'sm:is-authenticated()' |\
 ok "$( [ -n ${NGINX_HOME} ] )"  "nginx home set: ${NGINX_HOME}"
 
 ok "$( [[ -n "$(curl -I -s -f 'http://127.0.0.1:80/')" ]] )"  'nginx is reachable'
+
+is "$(curl -s http://example.com/test.html | grep -oP 'Powered by Jetty')" \
+ 'Powered by Jetty' \
+ 'example.com Powered by Jetty' 
+
 
 note "FIN"
 
