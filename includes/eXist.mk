4568e3a3708f55988188d5998180ce7c1f9446f6
grantmacken <grantmacken@gmail.com>
2016-01-20 15:53:19 +1300

    reintroduce deploy

--- includes/eXist.mk
+++ includes/eXist.mk
@@ -171,7 +171,7 @@ $(T)/download_url.txt:
 	@echo "{{{## $(notdir $@) ##"
 	@curl -s x https://api.github.com/repos/$(DEPLOY)/releases/latest | \
  jq '.assets[] | .browser_download_url'  >> $@
-	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
+	@$(call chownToUser,$(@))
 	@cat $@
 	@echo '}}}'
 
@@ -181,7 +181,7 @@ $(T)/deploy.sh: $(T)/download_url.txt
 	@echo 'cd $(EXIST_HOME)' >> $(@)
 	@echo "echo \"repo:install-and-deploy('$(WEBSITE)','$(shell cat $<)')\" | \\" >> $@
 	@echo ' java -jar $(EXIST_HOME)/start.jar client -sqx -u admin -P $(P) | tail -1' >> $@
-	@$(if $(SUDO_USER),chown $(SUDO_USER)$(:)$(SUDO_USER) $(@),)
+	@$(call chownToUser,$(@))
 	@chmod +x $(@)
 	@cat $@
 	@echo '}}}'
