16e45099adbef836d57ed4c989a3c85ff1a95e45
grantmacken <grantmacken@gmail.com>
2016-08-31 13:21:57 +1200

    clone letsencrypt.sh

--- includes/openresty.mk
+++ includes/openresty.mk
@@ -362,7 +362,8 @@ endef
 orLE: export orLetsEncryptConf:=$(ngSimporSimpleConf)
 orLE:
 	@echo "$${orLetsEncryptConf}" > $(NGINX_HOME)/conf/letsencrypt.conf
-	@[ -d /opt/letsencrypt.sh ] || git clone https://github.com/lukas2511/letsencrypt.sh
+	@[ -d /opt/letsencrypt.sh ] || \
+ cd /opt/letsencrypt.sh; git clone https://github.com/lukas2511/letsencrypt.sh
 	@cd /opt/letsencrypt.sh && cp docs/examples/config config
 	@cat /opt/letsencrypt.sh/ && cp config
 
