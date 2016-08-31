7b64d5067abca54803eb5146f430524bd40df876
grantmacken <grantmacken@gmail.com>
2016-08-31 14:44:12 +1200

    clone letsencrypt.sh

--- includes/openresty.mk
+++ includes/openresty.mk
@@ -362,12 +362,8 @@ endef
 orLE: export orLetsEncryptConf:=$(ngSimporSimpleConf)
 orLE:
 	@echo "$${orLetsEncryptConf}" > $(NGINX_HOME)/conf/letsencrypt.conf
-	@[ -e '/opt/letsencrypt.sh/letsencrypt.sh' ] || \
- git clone https://github.com/lukas2511/letsencrypt.sh /opt  && \
- echo 'letsencrypt installed'
-	[ -d '/opt/letsencrypt.sh/.acme-challenges' ] || \
- mkdir '/opt/letsencrypt.sh/.acme-challenges' && \
- echo 'challanges dir created'
+	@[ -e /opt/letsencrypt.sh/letsencrypt.sh ] ||  git clone https://github.com/lukas2511/letsencrypt.sh /opt/letsencrypt.sh/
+	[ -d /opt/letsencrypt.sh/.acme-challenges ] ||  mkdir /opt/letsencrypt.sh/.acme-challenges
 	@echo 'gmack.nz' > /opt/letsencrypt.sh/domains.txt
 	@echo 'CONTACT_EMAIL=grantmacken@gmail.com' > /opt/letsencrypt.sh/config
 
